ebc6fc07c13d778a8c7e8427746d07e7455295bf
ben-lehman <ben.a.lehman@gmail.com>
2025-03-20 16:07:30 -0700

    added config setup functionality

---
+++ internal/config/config.go
@@ -0,0 +1,73 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+)
+
+const configFileName = "/.gorssconfig.json"
+
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUsername string `json:"current_user_name"`
+}
+
+func Read() (Config, error) {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+    return Config{}, fmt.Errorf("issue opening file: %v", err)
+	}
+	defer file.Close()
+
+  byteVal, err := io.ReadAll(file)
+  if err != nil {
+    return Config{}, fmt.Errorf("issue reading file: %v", err)
+  }
+
+	var config Config
+	err = json.Unmarshal(byteVal, &config)
+	if err != nil {
+    return config, fmt.Errorf("issue decoding: %v", err)
+	}
+
+	return config, nil
+}
+
+func (c *Config) SetUser(username string) error {
+	c.CurrentUsername = username
+
+	configPath, err := getConfigFilePath()
+	if err != nil {
+    fmt.Printf("Issue getting config path: %v", err)
+		return err
+	}
+	jsonData, err := json.MarshalIndent(c, "", "  ")
+  if err != nil {
+    fmt.Printf("Issue marshaling data: %v", err)
+    return err
+  }
+
+  err = os.WriteFile(configPath, jsonData, 0644)
+  if err != nil {
+    fmt.Printf("Issue writing file: %v", err)
+    return err
+  }
+
+  fmt.Println("json data written to ", configPath)
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return userHomeDir + configFileName, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/ben-lehman/gorss/internal/config"
+)
 func main() {
+  conf, err := config.Read()
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+  }
+  conf.SetUser("ben")
+  conf, err = config.Read()
+  if err != nil {
+    fmt.Printf("Error: %v\n", err)
+  }
+  fmt.Printf("db url: %v\ncurrent user: %v\n", conf.DbURL, conf.CurrentUsername)
   return
 }
