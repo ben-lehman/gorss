7c6aaf78e6a41d679b9f1582ea2f75d830422b19
ben-lehman <ben.a.lehman@gmail.com>
2025-03-21 14:28:04 -0700

    Added database setup, schema and migrations. Added register user command

--- commands.go
+++ commands.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/ben-lehman/gorss/internal/database"
+	"github.com/google/uuid"
 )
 
 
@@ -33,10 +38,39 @@ func handlerLogin(state *State, cmd command) error {
 	}
 
   username := cmd.args[0]
-	err := state.cfg.SetUser(username)
+ 
+  _, err := state.db.GetUser(context.Background(), username)
+  if err != nil {
+    return fmt.Errorf("No user with that name: %v", err)
+  }
+
+	err = state.cfg.SetUser(username)
 	if err != nil {
 		return err
 	}
 	fmt.Printf("db url: %v\ncurrent user: %v\n", state.cfg.DbURL, state.cfg.CurrentUsername)
 	return nil
+}
+
+func handlerRegister(state *State, cmd command) error {
+  if len(cmd.args) != 1 {
+    return fmt.Errorf("invalid login format. usage: %s <name>", cmd.name)
+  }
+
+  username := cmd.args[0]
+  dbUser := database.CreateUserParams{
+    ID: uuid.New(),
+    CreatedAt: time.Now(),
+    UpdatedAt: time.Now(),
+    Name: username,
+  }
+
+  user, err := state.db.CreateUser(context.Background(), dbUser)
+  if err != nil {
+    return err
+  }
+
+  state.cfg.SetUser(username)
+  fmt.Printf("User was created: %v", user)
+  return nil 
 }
--- main.go
+++ main.go
@@ -1,45 +1,58 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/ben-lehman/gorss/internal/config"
+	"github.com/ben-lehman/gorss/internal/database"
+	_ "github.com/lib/pq"
 )
 
 type State struct {
+  db *database.Queries
 	cfg *config.Config
 }
 
 func main() {
 	conf, err := config.Read()
 	if err != nil {
-    log.Fatalf("Error reading config file: %v", err)
+		log.Fatalf("Error reading config file: %v", err)
 	}
+	dbConnectionURL := conf.DbURL
+	db, err := sql.Open("postgres", dbConnectionURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+  defer db.Close()
+	dbQueries := database.New(db)
+  
 	state := State{
+    db: dbQueries,
 		cfg: &conf,
 	}
-
 	commands := commands{
 		registeredCommands: make(map[string]func(*State, command) error),
 	}
 
-  commands.register("login", handlerLogin)
+	commands.register("login", handlerLogin)
+  commands.register("register", handlerRegister)
 
-  args := os.Args
-  if len(args) < 2 {
-    log.Fatal("Usage: cli <command> [args...]")
-    return
-  }
+	args := os.Args
+	if len(args) < 2 {
+		log.Fatal("Usage: cli <command> [args...]")
+		return
+	}
 
-  command := command{
-    name: args[1],
-    args: args[2:],
-  }
-  
-  err = commands.run(&state, command)
-  if err != nil {
-    log.Fatal(err)
-  }
+	command := command{
+		name: args[1],
+		args: args[2:],
+	}
+
+	err = commands.run(&state, command)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return
 }
